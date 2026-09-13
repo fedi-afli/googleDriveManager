99aa63bd45aca96db8d3bf9205ffe8c76045c51e
fedi-afli <f3diafli@gmail.com>
2026-09-13 16:04:45 +0100

    create project stucture

---
+++ google-drive-manager-backend/src/main/java/com/googledrive/googleDriveManager/GoogleDriveManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.googledrive.googleDriveManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleDriveManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleDriveManagerBackendApplication.class, args);
+	}
+
+}
---
+++ google-drive-manager-backend/src/test/java/com/googledrive/googleDriveManager/GoogleDriveManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.googledrive.googleDriveManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleDriveManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
